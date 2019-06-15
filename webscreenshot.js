56ec7d64b62b5d1f6d3b81e551e1acd80587537b
Manuel Bua <manuel.bua@gmail.com>
2019-06-15 17:21:53 +0200

    Raise Chrome UA version to 73.x, avoid deprecated browser nagscreens

--- webscreenshot.js
+++ webscreenshot.js
@@ -37,7 +37,7 @@ var Page = (function(custom_headers, http_username, http_password, image_width, 
 		height: opts.height
 	};
 	
-	page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36';
+	page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36';
 	page.settings.userName = http_username;
 	page.settings.password = http_password;
 	
