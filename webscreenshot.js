bfbe01b09613122b4d563c842e230102466fa162
Manuel Bua <manuel.bua@gmail.com>
2019-06-15 17:55:47 +0200

    Add clipping rect when rasterizing, ensure image height can't raise much

--- webscreenshot.js
+++ webscreenshot.js
@@ -37,6 +37,13 @@ var Page = (function(custom_headers, http_username, http_password, image_width, 
 		height: opts.height
 	};
 
+	page.clipRect = {
+		top: 0,
+		left: 0,
+		width: opts.width,
+		height: opts.height * 2
+	};
+
 	page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36';
 	page.settings.userName = http_username;
 	page.settings.password = http_password;
